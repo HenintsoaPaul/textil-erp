5bdf40d1a41317f4f478abbaea4c229ca4c3d9cf
henintsoapaul <henintsoapaul@gmail.com>
2026-04-19 18:54:56 +0300

    feat: auth guard

--- src/app/features/core/layout/admin-layout.component.ts
+++ src/app/features/core/layout/admin-layout.component.ts
@@ -1,53 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-// import { AuthService } from '../../../../services/auth.service';
-// import { CartService } from '../../services/cart.service';
-// import { WishlistService } from '../../services/wishlist.service';
-// import { UserProfileComponent } from '../../../../shared/components/user-profile/user-profile.component';
-// import { SnackbarComponent } from '../../../../shared/components/snackbar/snackbar.component';
-// import { FooterComponent } from '../../../../shared/components/footer/footer.component';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule,
-    // UserProfileComponent, SnackbarComponent, FooterComponent
-  ]
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule]
 })
 export class AdminLayoutComponent implements OnInit {
+  private authService = inject(AuthService);
   showProfileModal = false;
 
   appName = 'Textil ERP';
   panelName = 'Admin Panel';
 
-  constructor(
-    // public authService: AuthService,
-    // public cartService: CartService,
-    // public wishlistService: WishlistService
-  ) { }
+  currentUser = this.authService.currentUser;
+
+  constructor() { }
 
   ngOnInit() {
-    // this.cartService.loadCart().subscribe();
-    // this.wishlistService.loadWishlist().subscribe();
   }
 
   logout() {
-    // this.authService.logout();
+    this.authService.logout();
   }
 
   toggleProfileModal() {
     this.showProfileModal = !this.showProfileModal;
   }
-
-  // get userName(): string {
-  //   return this.authService.currentUserValue?.name || 'Customer';
-  // }
-
-  // get userProfileImage(): string {
-  //   return this.authService.currentUserValue?.profileImage || '';
-  // }
 
   navs = [
     {
